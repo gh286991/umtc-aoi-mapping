3399b91df60997b56e0c8c93a883e36bce5dd18d
Tom <gh286991@gmail.com>
2021-07-07 00:34:04 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,23 +2,40 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const styles = {
+  container:{
+    padding: 20,
+  },
+  fileContainer: {
+    padding: 20,
+  }
+}
 function App() {
+
+  const getFiles = (event:any) => {
+    let files = event.target.files;
+    let listing = document.getElementById("listing");
+    for (let i = 0; i < files.length; ++i) {
+      let item = document.createElement("li");
+      item.innerHTML = files[i].webkitRelativePath;
+      listing?.appendChild(item);
+    }
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div style={styles.container}>
+       <input
+        multiple
+        /* @ts-expect-error */
+        directory="" 
+        webkitdirectory=""
+        type="file"
+        onChange={(event) => {
+          getFiles(event);
+        }}
+      />
+      <div style={styles.fileContainer} id="listing" ></div>
+      </div>
     </div>
   );
 }
